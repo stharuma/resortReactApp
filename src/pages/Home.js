9046ce8c6a84e60488ffafee92af83bf801af642
stharuma <stharuma@gmail.com>
2019-06-11 11:26:23 -0400

    created services component and added services to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
-import { Link } from "react-router-dom";
+import Services from "../components/Services";
+
 const Home = () => {
   return (
-    <Hero>
-      <Banner title="Luxurious rooms" subtitle="deluxe rooms starting at $299">
-        <Link to="/rooms" className="btn-primary">
-          Our Rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner
+          title="Luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            Our Rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
   );
 };
 
