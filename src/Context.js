d21ea9b9171b305081c0fd0c7bcb8d5a64404d6b
stharuma <stharuma@gmail.com>
2019-06-11 13:47:19 -0400

    added React Context API

---
+++ src/Context.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+const RoomContext = React.createContext();
+class RoomProvider extends Component {
+  state = {
+    greeting: "Hello",
+    name: "Suresh"
+  };
+  render() {
+    return (
+      <RoomContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </RoomContext.Provider>
+    );
+  }
+}
+
+const RoomConsumer = RoomContext.Consumer;
+export { RoomProvider, RoomContext, RoomConsumer };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import Services from "../components/Services";
+import FeaturedRooms from '../components/FeaturedRooms';
 
 const Home = () => {
   return (
@@ -19,6 +20,7 @@ const Home = () => {
         </Banner>
       </Hero>
       <Services />
+      <FeaturedRooms/>
     </>
   );
 };
