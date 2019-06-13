627a2b9ebf7da233bf1186b93984808740dc7764
stharuma <stharuma@gmail.com>
2019-06-13 12:56:49 -0400

    validated room fields using PropTypes

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import defaultImg from "../images/room-1.jpeg";
+import PropTypes from "prop-types";
+
 export default function Room({ room }) {
   const { name, slug, images, price } = room;
 
@@ -20,3 +22,13 @@ export default function Room({ room }) {
     </article>
   );
 }
+
+Room.propTypes = {
+  room:PropTypes.shape({
+    name:PropTypes.string.isRequired,
+    slug:PropTypes.string.isRequired,
+    images:PropTypes.arrayOf(PropTypes.string).isRequired,
+    price:PropTypes.number.isRequired
+
+  })
+};
