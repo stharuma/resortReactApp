46236f3b9712872b436ce866c6f7b58309597c67
stharuma <stharuma@gmail.com>
2019-06-14 14:19:16 -0400

    updated StyledComponent with styledComponent and addded to SingleRoom

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -4,42 +4,54 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../Context";
+import StyledHero from '../components/StyledHero'; 
 
 export default class SingleRoom extends Component {
   constructor(props) {
     super(props);
-   // console.log("props",this.props);
-    this.state ={
-      slug:this.props.match.params.slug,
+    // console.log("props",this.props);
+    this.state = {
+      slug: this.props.match.params.slug,
       defaultBcg
-    }
+    };
   }
- static contextType = RoomContext;
+  static contextType = RoomContext;
   //componentDidMount() {}
 
   render() {
-    const {getRoom} = this.context;
-    const room =getRoom(this.state.slug);
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
     //console.log(room);
-    if(!room){
-      return <div className="error">
-      <h4>no such room could not found....</h4>
-      <Link to='/rooms' className="btn-primary">
-      back to rooms
-      </Link>
-      </div>
-    }
-    const {name, description, capacity, size, price, extras, brakfast,pets, images}= room;
-    
-    return( 
-      <Hero className="roomsHero">
-      <Banner title={`${name} room`}>
-      <Link to='/rooms' className="btn-primary">
-      back to rooms
-      </Link>
-      </Banner>
-
-      </Hero>
+    if (!room) {
+      return (
+        <div className="error">
+          <h4>no such room could not found....</h4>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </div>
       );
-   }
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images
+    } = room;
+
+    return (
+      <StyledHero img={images[0] || this.state.defaultBcg}>
+        <Banner title={`${name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+      </StyledHero>
+    );
+  }
 }
